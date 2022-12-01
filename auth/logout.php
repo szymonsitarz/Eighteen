9480501b48fedffd425c932509851cfb0b6f7fc5
Harry Wharton <200219998@aston.ac.uk>
2022-12-01 20:00:18 +0000

    fixed logout.php

--- auth/logout.php
+++ auth/logout.php
@@ -2,10 +2,10 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/shared-files/200219998/db.php');
 
+$_SESSION = array();
+unset($_SESSION);
+session_unset();
 session_destroy();
-foreach($_SESSION as $tem){
-    unset($tem);
-}
 
 header("Location: /home/home.php.php");
 
